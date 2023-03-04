3a76805e744648ec1b3ec4a24a6b81fb1f783dba
Adam Malosh <adam@tiltify.com>
2023-03-03 16:49:00 -0800

    try this?

--- mix.exs
+++ mix.exs
@@ -97,7 +97,8 @@ defmodule Nostrum.Mixfile do
   defp deps do
     [
       {:jason, "~> 1.2"},
-      {:gun, "~> 2.0"},
+      {:cowlib, "~> 2.11", override: true},
+      {:gun, "== 2.0.0", hex: :remedy_gun},
       {:certifi, "~> 2.8"},
       {:kcl, "~> 1.4"},
       {:mime, "~> 1.6 or ~> 2.0"},
