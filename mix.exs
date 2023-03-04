1dcced8ffdda9aa947c7088c595d8018591af6bd
Adam Malosh <adam@tiltify.com>
2023-03-03 16:42:49 -0800

    gun 2.0

--- mix.exs
+++ mix.exs
@@ -97,8 +97,7 @@ defmodule Nostrum.Mixfile do
   defp deps do
     [
       {:jason, "~> 1.2"},
-      {:cowlib, "~> 2.11.1", hex: :remedy_cowlib},
-      {:gun, "== 2.0.1", hex: :remedy_gun},
+      {:gun, "~> 2.0"},
       {:certifi, "~> 2.8"},
       {:kcl, "~> 1.4"},
       {:mime, "~> 1.6 or ~> 2.0"},
